6468ebd1c59b1a8433596daf3a8191ac74c71656
vincensromain <vincensromainpro@gmail.com>
2025-02-23 11:44:57 +0100

    update test for vercel

--- src/app/globals.jsx
+++ src/app/globals.jsx
@@ -8,7 +8,7 @@ import Intro from "../components/Intro/Intro";
 import Le_jardin from "../components/Le_jardin/Le_jardin";
 import Le_jardin_2 from "../components/Le_jardin_vue_2/Le_jardin_vue_2";
 import Le_jardin_3 from "../components/Le_jardin_vue_3/Le_jardin_vue_3";
-import Article_lavande from "../components/Article_Lavande/Article_Lavande";
+import Article_lavande from "../components/Article_lavande/Article_Lavande";
 import Menu from "../components/Menu/Menu";
 
 createRoot(document.getElementById("root")).render(
